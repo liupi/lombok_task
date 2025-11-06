72bbb39b0b773cfdd4ad4f252786d95ee4bd5094
Liudmila <milalindley@gmail.com>
2025-11-07 00:43:24 +0100

    feat: exclude some fields in @EqualsAndHashCode

--- src/lmb/CarWithLombok.java
+++ src/lmb/CarWithLombok.java
@@ -7,7 +7,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@EqualsAndHashCode(exclude = {"model", "year", "price", "color", "quantity"})
 public class CarWithLombok {
     private String model;
     private String brand;
